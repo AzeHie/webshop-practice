d3014eba026c04db3ec400799807eaa266971eda
AzeHie <109618459+AzeHie@users.noreply.github.com>
2022-09-05 19:00:15 +0300

    Add files via upload

---
+++ src/app/auth/auth-data.module.ts
@@ -0,0 +1,10 @@
+export interface AuthData {
+  id: string;
+  firstname: string;
+  lastname: string;
+  address: string;
+  postcode: string;
+  city: string;
+  email: string;
+  password: string;
+}
