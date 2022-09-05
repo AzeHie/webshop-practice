85e2016f669b50d04d4679f5445eea1aa243dc44
AzeHie <109618459+AzeHie@users.noreply.github.com>
2022-09-05 18:45:51 +0300

    Add files via upload

---
+++ src/app/products/accessories/accessories-detail/accessories-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-accessories-detail',
+  templateUrl: './accessories-detail.component.html',
+  styleUrls: ['./accessories-detail.component.css']
+})
+export class AccessoriesDetailComponent {
+
+}
