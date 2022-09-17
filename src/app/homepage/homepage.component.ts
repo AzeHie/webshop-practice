74436b4484d2522d579f91b18a0bc3aa846b0dc1
AzeHie <109618459+AzeHie@users.noreply.github.com>
2022-09-17 14:18:16 +0300

    Update homepage.component.ts
    Fixing code dublications. Wunderbaum-model replaced with product-model.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
-import { AccessoriesService } from '../services/accessories.service';
 import { ClothesService } from '../services/clothes.service';
 import { Product } from '../products/product-model';
-import { Wunderbaum } from '../products/wunderbaums/wunderbaum.model';
 import { WunderbaumsService } from '../services/wunderbaums.service';
 
 @Component({
@@ -13,16 +11,14 @@ import { WunderbaumsService } from '../services/wunderbaums.service';
   styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit, OnDestroy {
-  wunderbaums: Wunderbaum[] = [];
+  wunderbaums: Product[] = [];
   clothes: Product[] = [];
   private wunderbaumsSub: Subscription;
   private clothesSub: Subscription;
-  private accessoriesSub: Subscription;
 
   constructor(
     private wunderbaumsService: WunderbaumsService,
-    private clothesService: ClothesService,
-    private accessoriesService: AccessoriesService
+    private clothesService: ClothesService
   ) {}
 
   ngOnInit(): void {
@@ -31,7 +27,7 @@ export class HomepageComponent implements OnInit, OnDestroy {
     // this.accessoriesService.getAccessories();
     this.wunderbaumsSub = this.wunderbaumsService
       .getWunderbaumUpdateListener()
-      .subscribe((resData: { wunderbaums: Wunderbaum[] }) => {
+      .subscribe((resData: { wunderbaums: Product[] }) => {
         this.wunderbaums = resData.wunderbaums;
       });
     this.clothesSub = this.clothesService
