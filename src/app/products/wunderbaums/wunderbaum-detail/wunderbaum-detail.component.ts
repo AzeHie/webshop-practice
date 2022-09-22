f8f5c96c9c0b82810d88aa2ef57c538f2a8e4379
AzeHie <109618459+AzeHie@users.noreply.github.com>
2022-09-22 19:57:54 +0300

    Update wunderbaum-detail.component.ts

--- src/app/products/wunderbaums/wunderbaum-detail/wunderbaum-detail.component.ts
+++ src/app/products/wunderbaums/wunderbaum-detail/wunderbaum-detail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
-import { WunderbaumsService } from '../../../services/wunderbaums.service';
 import { Subscription } from 'rxjs';
 import { Product } from '../../product-model';
 
