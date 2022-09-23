5274b0604113cc0d0f5209360dd67a9ed6eed286
AzeHie <109618459+AzeHie@users.noreply.github.com>
2022-09-23 18:09:47 +0300

    Update orders.js
    then blocks replaced with async

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -1,22 +1,21 @@
 const Order = require("../models/order");
 
-exports.saveOrder = (req, res, next) => {
+exports.saveOrder = async (req, res, next) => {
   const order = new Order({
     products: req.body.products,
     customer: req.body.customerDetails,
     shippingMethod: req.body.shippingMethod,
     paymentMethod: req.body.paymentMethod,
   });
-  order
-    .save()
-    .then(() => {
-      res.status(201).json({
-        message: "Order saved successfully!",
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Please, check details and try again!",
-      });
+
+  try {
+    await order.save();
+    res.status(201).json({
+      message: "Order saved successfully!"
     });
+  } catch (err) {
+    res.status(500).json({
+      message: "Something went wrong when sending the order! Please, check the details!",
+    });
+  };
 };
