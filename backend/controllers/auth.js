15aa7ab20e2fa9103149ee59f4fc909dae0f37f9
AzeHie <109618459+AzeHie@users.noreply.github.com>
2022-09-23 18:07:13 +0300

    Update auth.js
    then blocks replaced with async

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,110 +3,108 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
-exports.createUser = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10) // encrypting password
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        address: req.body.address,
-        postcode: req.body.postcode,
-        city: req.body.city,
-      });
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({
-            message: "New user added successfully",
-          });
-        })
-        .catch((error) => {
-          res.status(500).json({
-            message: "Authentication failed!",
-          });
-        });
-    });
-};
+exports.createUser = async (req, res, next) => {
+  try {
+    const hash = await bcrypt.hash(req.body.password, 10); // encrypting password
 
-exports.userLogin = (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({
-          message: "Invalid username or password!",
-        });
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then((result) => {
-      if (!result) {
-        return res.status(401).json({
-          message: "Invalid username or password!",
-        });
-      }
-      const token = jwt.sign(
-        //creating json webtoken, has to be installed with cli
-        { email: fetchedUser.email, userId: fetchedUser._id },
-        process.env.JWT_KEY,
-        { expiresIn: "1h" }
-      );
-      res.status(200).json({
-        token: token,
-        expiresIn: 3600,
-        userId: fetchedUser._id,
-        firstname: fetchedUser.firstname,
-        lastname: fetchedUser.lastname,
-        address: fetchedUser.lastname,
-        postcode: fetchedUser.postcode,
-        city: fetchedUser.city,
-        email: fetchedUser.email,
-      });
-    })
-    .catch((error) => {
-      return res.status(401).json({
-        message: "Invalid authetication credentials!",
-      });
-    });
-};
-
-exports.updateUser = (req, res, next) => {
-  let doc = User.findOneAndUpdate(
-    { _id: req.userData.userId }, // FILTER
-    {
-      firstname: req.body.firstname, // UPDATE
+    const user = new User({
+      email: req.body.email,
+      password: hash,
+      firstname: req.body.firstname,
       lastname: req.body.lastname,
       address: req.body.address,
       postcode: req.body.postcode,
       city: req.body.city,
-      email: req.body.email,
-    },
-    { returnDocument: "after" } // RETURN UPDATED DOC
-  );
-  doc
-    .then((updatedUser) => {
-      res.status(200).json({
-        message: "User updated!",
-        userDetails: {
-          id: updatedUser._id,
-          firstname: updatedUser.firstname,
-          lastname: updatedUser.lastname,
-          address: updatedUser.address,
-          postcode: updatedUser.postcode,
-          city: updatedUser.city,
-          email: updatedUser.email,
-        },
-      });
-    })
-    .catch((error) => {
-      return res.status(500).json({
-        message: "Updating user failed!",
-      });
     });
+
+    await user.save();
+
+    res.status(201).json({
+      message: "New user added successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Something went wrong, check the details and try again.",
+    });
+  }
+};
+
+exports.userLogin = async (req, res, next) => {
+  let fetchedUser;
+
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if(!user) {
+      return res.status(401).json({
+        message: "Invalid username or password!"
+      });
+    }
+    fetchedUser = user;
+
+    const passwordsMatch = await bcrypt.compare(req.body.password, user.password);
+    if(!passwordsMatch) {
+      return res.status(401).json({
+        message: "Invalid username or password!"
+      });
+    }
+
+    //creating json webtoken, has to be installed with cli:
+    const token = jwt.sign(
+      { email: fetchedUser.email, userId: fetchedUser._id },
+      process.env.JWT_KEY, // found from nodemon.js
+      { expiresIn: "1h" }
+    );
+    return res.status(200).json({
+      token: token,
+      expiresIn: 3600,
+      userId: fetchedUser._id,
+      firstname: fetchedUser.firstname,
+      lastname: fetchedUser.lastname,
+      address: fetchedUser.lastname,
+      postcode: fetchedUser.postcode,
+      city: fetchedUser.city,
+      email: fetchedUser.email,
+    })
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid authentication credentials!"
+    });
+  };
+};
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    const query = await User.findOneAndUpdate(
+      { _id: req.userData.userId }, // FILTER
+      {
+        firstname: req.body.firstname, // UPDATE
+        lastname: req.body.lastname,
+        address: req.body.address,
+        postcode: req.body.postcode,
+        city: req.body.city,
+        email: req.body.email,
+      },
+      { returnDocument: "after" } // RETURN UPDATED DOC
+    );
+    const updatedUser = query;
+
+    return res.status(200).json({
+      message: "User updated!",
+      userDetails: {
+        id: updatedUser._id,
+        firstname: updatedUser.firstname,
+        lastname: updatedUser.lastname,
+        address: updatedUser.address,
+        postcode: updatedUser.postcode,
+        city: updatedUser.city,
+        email: updatedUser.email,
+      }
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Updating the user failed!"
+    });
+  };
 };
 
 exports.changePassword = async (req, res, next) => {
